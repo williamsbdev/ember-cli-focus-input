a61229a16bce2a3b8d02d106b1656531b3add419
armin <armin@wurzweb.com>
2019-03-24 14:23:48 +0100

    NEW: update addon to Ember 3.8 LTS
    # Conflicts:
    #	.eslintignore
    #	.gitignore
    #	.npmignore
    #	.travis.yml
    #	LICENSE.md
    #	README.md
    #	config/ember-try.js
    #	index.js

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,55 +1,16 @@
-/*jshint node:true*/
-module.exports = {
-  scenarios: [
-    {
-      name: 'default',
-      bower: {
-        dependencies: { }
-      }
-    },
-    {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
+'use strict';
+
+module.exports = function() {
+  return {
+      scenarios: [
+        {
+          name: 'ember-lts-2.18',
+          npm: {
+            dependencies: {
+              'ember-source': '~2.18.0'
+            }
+          }
         }
-      }
-    },
-    {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      }
-    }
-  ]
+      ]
+  };
 };
