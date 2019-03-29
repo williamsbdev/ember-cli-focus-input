7ebfb28a3e49bc3a1138146fa321c4c952d4979f
armin <armin@wurzweb.com>
2019-03-29 10:26:52 +0100

    CHG: remove unnecassary .eslintrc from tests dir. add es6 module imports.

--- tests/dummy/app/routes/input.js
+++ tests/dummy/app/routes/input.js
@@ -1,7 +1,8 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
 
-export default Ember.Route.extend({
-    model: function() {
-        return Ember.Object.create({name: 'foo'});
-    }
+export default Route.extend({
+  model() {
+    return EmberObject.create({name: 'foo'});
+  }
 });
